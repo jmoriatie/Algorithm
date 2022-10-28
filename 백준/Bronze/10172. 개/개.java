24cb690421b5a448d24cf8cfda25622a838c290e
jmoriatie <80316985+jmoriatie@users.noreply.github.com>
2022-10-28 23:44:13 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11296 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+public class Main {
+	// 시작 : 09:15
+	// 종료 : 09:37
+	// 소요 : 00:22
+    // 출력오류, 런타임 오류로 인한 시간 지연
+	public static void main(String[] args) {
+		System.out.print("|\\_/|\n");
+		System.out.print("|q p|   /}\n");
+		System.out.print("( 0 )\"\"\"\\\n");
+		System.out.print("|\"^\"`    |\n");
+		System.out.print("||_/=\\\\__|\n");
+		
+	}
+}
