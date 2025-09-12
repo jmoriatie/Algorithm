1dc38d561e659e784455ef6fc5b734d66c9067fb
JeongMin <80316985+jmoriatie@users.noreply.github.com>
2025-09-12 21:28:03 +0900

    [Bronze V] Title: A+B - 4, Time: 68 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,37 +1,25 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-//1 1
-//2 3
-//3 4
-//9 8
-//5 2
 public class Main {
-	// 시작 : 10:05
-	// 종료 : 10:08
-	// 소요 : 00:03
-	public static void main(String[] args) throws IOException { 
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String in;
-		StringTokenizer st;
-		
-		while((in = br.readLine()) != null) {
-			st = new StringTokenizer(in, " "); 
-			int a = Integer.parseInt( st.nextToken() );
-			int b = Integer.parseInt( st.nextToken() );
-			bw.write( (a+b) + "\n" );
-		}
 
-		br.close();
-		bw.flush(); 
-		bw.close();
+    public static int plus(int a, int b) {
+        return a + b;
+    }
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numbers = br.readLine().split(" ");
+        boolean bool = br.ready();
+        while (bool) {
+            int n1 = Integer.parseInt(numbers[0]);
+            int n2 = Integer.parseInt(numbers[1]);
+            System.out.println(plus(n1, n2));
+
+            bool = br.ready();
+            if(bool)numbers = br.readLine().split(" ");
+        }
+    }
 }
