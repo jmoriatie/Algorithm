dc6b08ff95926f2c3a588d3bb8f5c780c5e1a8c7
JeongMin <80316985+jmoriatie@users.noreply.github.com>
2025-09-12 21:03:32 +0900

    [Bronze V] Title: A+B - 3, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int plus(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        for(int i=0; i<cnt; i++){
+            String[] numbers = br.readLine().split(" ");
+            int n1 = Integer.parseInt(numbers[0]);
+            int n2 = Integer.parseInt(numbers[1]);
+            System.out.println(plus(n1, n2));
+        }
+    }
+}
