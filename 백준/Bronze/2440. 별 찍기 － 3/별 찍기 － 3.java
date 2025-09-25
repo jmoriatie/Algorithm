b23baacd20c5772288698e3bae8589c060707185
JeongMin <80316985+jmoriatie@users.noreply.github.com>
2025-09-26 06:06:04 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 120 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void makeStar(int begin, int end){
+        if(begin < 1) return;
+
+        String star = "";
+        for(int i=1; i<=begin; i++){
+            star += "*";
+        }
+        if(!(begin == 1)) star += "\n";
+        System.out.print(star);        
+        makeStar(begin-1, end);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        makeStar(n, n);
+    }
+}
